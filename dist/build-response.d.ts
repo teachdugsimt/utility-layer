a13e9b2b938973d1408e4e9f5b8f12505375da62
Katanyoo Ubalee <ubalee.k@gmail.com>
2021-06-20 23:20:00 +0700

    chore: ignore module lock file

--- dist/build-response.d.ts
+++
@@ -1,8 +0,0 @@
-export default class BuildResponse {
-    buildErrorMessage(status: number, name: string, headers: any, userMessage: string, developerMessage: string, detail: any): {
-        isBase64Encoded: boolean;
-        statusCode: number;
-        headers: any;
-        body: string;
-    };
-}
--- dist/crypto-ramdom-string.d.ts
+++
@@ -1,7 +0,0 @@
-interface DataEntity {
-    length?: number;
-    type?: undefined | 'hex' | 'base64' | 'url-safe' | 'numeric' | 'distinguishable' | 'ascii-printable' | 'alphanumeric';
-    characters?: string;
-}
-declare const cryptoRandomString: (data?: DataEntity | undefined) => string;
-export default cryptoRandomString;
